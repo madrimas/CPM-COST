97e595553d6d8d46efb3086108ebeee9b75c84ea
madrimas <zapiorkowski@gmail.com>
2018-04-09 20:34:45 +0200

    add mockup

--- src/Event.java
+++ src/Event.java
@@ -1,4 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Event {
 
+    private static int eventID = 0;
+    private List<Integer> ingoingActionList;
+    private List<Integer> outgoingActionList;
 
+    Event(){
+        eventID++;
+        ingoingActionList = new ArrayList<>();
+        outgoingActionList = new ArrayList<>();
+    }
+
+    public int getEventID() {
+        return eventID;
+    }
+
+    public void setEventID(int eventID) {
+        this.eventID = eventID;
+    }
+
+    public List<Integer> getIngoingActionList() {
+        return ingoingActionList;
+    }
+
+    public void setIngoingActionList(List<Integer> ingoingActionList) {
+        this.ingoingActionList = ingoingActionList;
+    }
+
+    public List<Integer> getOutgoingActionList() {
+        return outgoingActionList;
+    }
+
+    public void setOutgoingActionList(List<Integer> outgoingActionList) {
+        this.outgoingActionList = outgoingActionList;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -15,6 +16,7 @@ public class Main {
         List<Double> afterTimeReductionList = globalData.getAfterTimeReductionList();
         List<Double> defaultCostList = globalData.getDefaultCostList();
         List<Double> reductionCostList = globalData.getReductionCostList();
+        List<Event> eventList = new ArrayList<>();
 
         for (int i = 0; i < numberOfActions; i++) {
             System.out.println("ID czynności: " + idList.get(i));
@@ -25,6 +27,31 @@ public class Main {
             System.out.println("Domyślny koszt: " + defaultCostList.get(i));
             System.out.println("Koszt skrócenia: " + reductionCostList.get(i));
             System.out.println();
+        }
+
+        for (int i = 0; i < numberOfActions; i++) {
+            eventList.add(new Event());
+            System.out.println("Event nr: " + eventList.get(i).getEventID());
+            for (int j = 0; j < numberOfActions; j++) {
+                Integer tempEventID = eventList.get(i).getEventID();
+                Integer tempActionID = eventStartList.get(j);
+                if (tempEventID.equals(tempActionID))
+                    eventList.get(i).getOutgoingActionList().add(idList.get(j));
+                tempActionID = eventEndList.get(j);
+                if (tempEventID.equals(tempActionID))
+                    eventList.get(i).getIngoingActionList().add(idList.get(j));
+            }
+        }
+
+        for (int i = 0; i < numberOfActions; i++) {
+            for (Integer object : eventList.get(i).getOutgoingActionList()
+                    ) {
+                System.out.println("Czynności wychodzące ze zdarzenia: " + (i+1) + " to: " + object);
+            }
+            for (Integer object : eventList.get(i).getIngoingActionList()
+                    ) {
+                System.out.println("Czynności wchodzące do zdarzenia: " + (i+1) + " to: " + object);
+            }
         }
     }
 }
